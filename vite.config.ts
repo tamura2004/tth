d396da3e6197f9a0789e374f03e47d56e3c10684
tamura2004 <tamura2004@gmail.com>
2025-01-17 13:16:39 +0000

    Add pwa settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      injectRegister: "auto", //ここの記述
+      manifest: {
+        name: "Through the Ages Counter",
+        short_name: "TtA Counter",
+        description: "スルージエイジズカウンター",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/tth/",
 });
